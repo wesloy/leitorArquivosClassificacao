6bdb42994cb6b273700712475ac53915d5014ad8
Wesley Eloy <52499714+wesloy@users.noreply.github.com>
2020-08-07 16:25:14 -0300

    Adicionar arquivos de projeto.

---
+++ leitorArquivosClassificacao/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace leitorArquivosClassificacao {
+    partial class frmPrincipal {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.txtCaminoNomePDF = new System.Windows.Forms.TextBox();
+            this.btnConverterPDF = new System.Windows.Forms.Button();
+            this.rTxt_texto = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ofd1
+            // 
+            this.ofd1.FileName = "ofd1";
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(501, 22);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcurar.TabIndex = 0;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // txtCaminoNomePDF
+            // 
+            this.txtCaminoNomePDF.Location = new System.Drawing.Point(23, 24);
+            this.txtCaminoNomePDF.Name = "txtCaminoNomePDF";
+            this.txtCaminoNomePDF.Size = new System.Drawing.Size(472, 20);
+            this.txtCaminoNomePDF.TabIndex = 1;
+            // 
+            // btnConverterPDF
+            // 
+            this.btnConverterPDF.Location = new System.Drawing.Point(23, 71);
+            this.btnConverterPDF.Name = "btnConverterPDF";
+            this.btnConverterPDF.Size = new System.Drawing.Size(75, 23);
+            this.btnConverterPDF.TabIndex = 2;
+            this.btnConverterPDF.Text = "Ler PDF";
+            this.btnConverterPDF.UseVisualStyleBackColor = true;
+            this.btnConverterPDF.Click += new System.EventHandler(this.btnConverterPDF_Click);
+            // 
+            // rTxt_texto
+            // 
+            this.rTxt_texto.Location = new System.Drawing.Point(23, 100);
+            this.rTxt_texto.Name = "rTxt_texto";
+            this.rTxt_texto.Size = new System.Drawing.Size(553, 337);
+            this.rTxt_texto.TabIndex = 3;
+            this.rTxt_texto.Text = "";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 449);
+            this.Controls.Add(this.rTxt_texto);
+            this.Controls.Add(this.btnConverterPDF);
+            this.Controls.Add(this.txtCaminoNomePDF);
+            this.Controls.Add(this.btnProcurar);
+            this.Name = "frmPrincipal";
+            this.Text = ".: Principal :.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofd1;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.TextBox txtCaminoNomePDF;
+        private System.Windows.Forms.Button btnConverterPDF;
+        private System.Windows.Forms.RichTextBox rTxt_texto;
+    }
+}
+
---
+++ leitorArquivosClassificacao/Form1.cs
@@ -0,0 +1,59 @@
+﻿
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace leitorArquivosClassificacao {
+    public partial class frmPrincipal : Form {
+        public frmPrincipal() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e) {
+            //define as propriedades do controle 
+            //OpenFileDialog
+            this.ofd1.Multiselect = false;
+            this.ofd1.Title = "Selecionar PDF";
+            ofd1.InitialDirectory = @"C:\dados";
+            //filtra para exibir somente arquivos de imagens
+            ofd1.Filter = "Files (*.PDF)|*.PDF|" + "All files (*.*)|*.*";
+            ofd1.CheckFileExists = true;
+            ofd1.CheckPathExists = true;
+            ofd1.FilterIndex = 2;
+            ofd1.RestoreDirectory = true;
+            ofd1.ReadOnlyChecked = true;
+            ofd1.ShowReadOnly = false;
+
+            DialogResult dr = this.ofd1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK) {
+                txtCaminoNomePDF.Text = ofd1.FileName;
+            }
+        }
+
+        private void btnConverterPDF_Click(object sender, EventArgs e) {
+
+            
+            iTextSharp.text.pdf.PdfReader leitor = new iTextSharp.text.pdf.PdfReader(txtCaminoNomePDF.Text);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <=  leitor.NumberOfPages; i++) {
+
+                sb.Append(PdfTextExtractor.GetTextFromPage(leitor, i));
+
+            }
+
+            rTxt_texto.Text = sb.ToString();
+            leitor.Close();
+
+        }
+
+
+
+
+    }
+}
+
